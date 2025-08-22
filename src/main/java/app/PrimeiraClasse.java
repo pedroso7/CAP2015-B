04e9434b96dff088cb92d290487ae79f379417b9
pedroso7 <viniciuspgarcia1@gmail.com>
2025-08-22 01:39:51 +0000

    Aula 1: Lombok 2025.08.21

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -2,6 +2,13 @@ package app;
 
 public class PrimeiraClasse {
     public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+        a1.setNome("Marco");
+        a1.setIdade(45);
+
+        System.out.println(a1.getNome());
+        System.out.println(a1.getIdade());
+
         System.out.println("Olá Gradle!!!");
     }
 }
